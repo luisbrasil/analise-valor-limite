e067ceca378418b0fdb83a702443eb39c8773757
Luis Fernando Brasil <luis4756@live.com>
2024-08-20 22:10:50 -0300

    Configurações proj

--- src/main/java/com/teste/Main.java
+++ src/main/java/com/teste/Main.java
@@ -1,7 +1,10 @@
 package com.teste;
 
 public class Main {
+
     public static void main(String[] args) {
+        SimuladorCashBack.calculaCashBack(100,"Bronze");
         System.out.println("Hello world!");
     }
+
 }
--- src/main/java/com/teste/SimuladorCashBack.java
+++ src/main/java/com/teste/SimuladorCashBack.java
@@ -11,8 +11,31 @@ public class SimuladorCashBack {
      * @return o valor de cashback calculado
      */
     public static double calculaCashBack(double valorCompra, String grauFidelidade) {
-        
-        
-        return 0;
+        double cashbackAdicional = 0;
+        double valorCashback = 0;
+
+        switch (grauFidelidade) {
+            case "Prata":
+                cashbackAdicional = 0.05;
+                break;
+
+            case "Ouro":
+                cashbackAdicional = 0.1;
+                break;
+
+            case "Bronze":
+            default:
+                break;
+        }
+
+        if (valorCompra < 99) {
+            valorCashback = valorCompra * (0.05 + cashbackAdicional);
+        } else if (valorCompra <= 500) {
+            valorCashback = valorCompra * (0.10 + cashbackAdicional);
+        } else {
+            valorCashback = valorCompra * (0.15 + cashbackAdicional);
+        }
+
+        return valorCashback;
     }
 }
