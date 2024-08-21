36986b9e0f443196c6dd2c02dd6ae8ba4e60ce60
Luis Fernando Brasil <luis4756@live.com>
2024-08-20 21:47:40 -0300

    Criação do Projeto Base

---
+++ src/main/java/com/teste/SimuladorCashBack.java
@@ -0,0 +1,18 @@
+package com.teste;
+
+public class SimuladorCashBack {
+    /**
+     * Calcula o valor de cashback baseado no valor da compra e no grau de
+     * fidelidade do cliente.
+     * 
+     * @param valorCompra    o valor total da compra
+     * @param grauFidelidade o grau de fidelidade do cliente (Bronze, Prata,
+     *                       Ouro)
+     * @return o valor de cashback calculado
+     */
+    public static double calculaCashBack(double valorCompra, String grauFidelidade) {
+        
+        
+        return 0;
+    }
+}
